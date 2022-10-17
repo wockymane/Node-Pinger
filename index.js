627d23b48338fefd009d32c992017628309fd33a
Pxzlz <91290196+Pxzlzz@users.noreply.github.com>
2022-10-17 18:00:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Group } = require('@hystleria/pinger');
 let groupPinger = new Group(); //You can pass a string to name the group if you have multiple groups
 
-['incognitoupdates.pxzlz.repl.co', 'taco.pxzlz.repl.co'].forEach(u => groupPinger.add(u, 60000));
+['incognitoupdates.pxzlz.repl.co/uptime.html', 'taco.pxzlz.repl.co'].forEach(u => groupPinger.add(u, 60000));
 groupPinger.startAll();
