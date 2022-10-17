97f861828e63ef80108a30ea7a74a6c67ee34573
Pxzlz <91290196+Pxzlzz@users.noreply.github.com>
2022-10-17 17:55:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-const pinger = require('@hystleria/pinger');
-pinger.ping("https://taco.pxzlz.repl.co", 60000)
+const { Group } = require('@hystleria/pinger');
+let groupPinger = new Group(); //You can pass a string to name the group if you have multiple groups
+
+['incognitoupdates.pxzlz.repl.co', 'taco.pxzlz.repl.co'].forEach(u => groupPinger.add(u, 60000));
+groupPinger.startAll();
